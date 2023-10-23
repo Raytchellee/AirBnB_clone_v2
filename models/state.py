e334363729e5f66410444b417888281d104f3490
Ray Ray <101654980+Raytchellee@users.noreply.github.com>
2023-10-23 12:10:18 +0100

    check task

--- models/state.py
+++ models/state.py
@@ -1,31 +1,30 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
-from models.base_model import BaseModel, Base
-from sqlalchemy import Column, Integer, String, DateTime, ForeignKey
-from sqlalchemy.orm import relationship
 import os
+from models.base_model import BaseModel, Base
+from sqlalchemy.orm import relationship
+from sqlalchemy import Column, Integer, String, DateTime, ForeignKey
 
 
 class State(BaseModel, Base):
-    """ State class """
+    """ class called State """
     __tablename__ = "states"
+
     name = Column(String(128), nullable=False)
-    cities = relationship("City",
-                          backref='state',
+    cities = relationship("City", backref='state',
                           cascade="all, delete-orphan",
                           single_parent=True)
 
     if os.getenv('HBNB_TYPE_STORAGE') != 'db':
-
         @property
         def cities(self):
-            """ getter method for cities """
+            """ returns cities in the state """
             from models import storage
-            cities = []
-            filestorage = storage._FileStorage__objects
-            for key, value in filestorage.items():
-                lista = key.split(".")
-                if lista[0] == "City":
-                    if value.to_dict()["state_id"] == self.id:
-                        cities.append(value)
-            return cities
+            city_list = []
+            store = storage._FileStorage__objects
+            for key, val in store.items():
+                parts = key.split(".")
+                if parts[0] == "City":
+                    if val.to_dict()["state_id"] == self.id:
+                        city_list.append(val)
+            return city_list
