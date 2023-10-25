c9eb16cb8616811ba1aca25dc7d1f33247b70818
UgonmaNkwocha <caramelinvestor@gmail.com>
2023-10-25 11:32:19 +0100

    Unittest updated

--- tests/test_console.py
+++ tests/test_console.py
@@ -9,21 +9,20 @@ HBNBCommand = console.HBNBCommand
 
 
 class TestConsoleDocs(unittest.TestCase):
-    """Class for testing documentation of the console"""
-
-    def test_console_module_docstring(self):
-        """Test for the console.py module docstring"""
-        self.assertIsNot(console.__doc__, None,
-                         "console.py needs a docstring")
-        self.assertTrue(len(console.__doc__) >= 1,
-                        "console.py needs a docstring")
-
+    """Class contains testing documentation of the command interpreter"""
     def test_HBNBCommand_class_docstring(self):
-        """Test for the HBNBCommand class docstring"""
+        """This tests for the HBNBCommand class docstring"""
         self.assertIsNot(HBNBCommand.__doc__, None,
                          "HBNBCommand class needs a docstring")
         self.assertTrue(len(HBNBCommand.__doc__) >= 1,
                         "HBNBCommand class needs a docstring")
+
+    def test_console_module_docstring(self):
+        """This tests for the console.py module docstring"""
+        self.assertIsNot(console.__doc__, None,
+                         "console.py needs a docstring")
+        self.assertTrue(len(console.__doc__) >= 1,
+                        "console.py needs a docstring")
 
 
 if __name__ == '__main__':
