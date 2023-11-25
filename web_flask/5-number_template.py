87d5ff9e6c4b10784faec4138d3a1350a0142631
raytchellee <amakaoforkaja@gmail.com>
2023-11-25 13:37:50 -0800

    task check

---
+++ web_flask/5-number_template.py
@@ -0,0 +1,46 @@
+#!/usr/bin/python3
+"""Starts Flask web application"""
+from flask import Flask, render_template
+
+flask_app = Flask(__name__)
+
+
+@flask_app.route("/", strict_slashes=False)
+def base():
+    """Returns Hello HBNB to request"""
+    return "Hello HBNB!"
+
+
+@flask_app.route("/hbnb", strict_slashes=False)
+def hbnb():
+    """Returns HBNB to /hbnb request"""
+    return "HBNB"
+
+
+@flask_app.route("/c/<text>", strict_slashes=False)
+def c_text(text):
+    """Returns HBNB to /c/<text> request"""
+    return "C %s" % str(text.replace("_", " "))
+
+
+@flask_app.route("/python/<text>", strict_slashes=False)
+@flask_app.route("/python/")
+def python_text(text="is cool"):
+    """Returns HBNB to /python/<text> request"""
+    return "Python %s" % str(text.replace("_", " "))
+
+
+@flask_app.route('/number/<int:n>', strict_slashes=False)
+def num(x):
+    """ confirms only numbers in /number/n """
+    return "{} is a number".format(x)
+
+
+@flask_app.route('/number_template/<int:n>', strict_slashes=False)
+def num_temp(x):
+    """ renders template on /number_template/n """
+    return render_template('5-number.html', number=x)
+
+
+if __name__ == "__main__":
+    flask_app.run(host="0.0.0.0", port="5000")
