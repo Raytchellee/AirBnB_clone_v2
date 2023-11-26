be5b6040c96eab552247cafbc12ec00aaeebd55d
raytchellee <amakaoforkaja@gmail.com>
2023-11-25 17:20:39 -0800

    check

--- web_flask/9-states.py
+++ web_flask/9-states.py
@@ -6,6 +6,7 @@ from models import storage
 
 flask_app = Flask(__name__)
 
+
 @flask_app.route('/states', strict_slashes=False)
 @flask_app.route('/states/<id>', strict_slashes=False)
 def get_states(id=None):
@@ -15,10 +16,12 @@ def get_states(id=None):
         id = 'State.' + id
     return render_template('9-states.html', state=s, id=id)
 
+
 @flask_app.teardown_appcontext
 def close_db(exep):
     """Close the db"""
     storage.close()
+
 
 if __name__ == '__main__':
     flask_app.run(host='0.0.0.0', port='5000')
