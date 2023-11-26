891c463c53d7ce3125501150547eed9909e35257
Ray Ray <101654980+Raytchellee@users.noreply.github.com>
2023-11-26 02:07:48 +0100

    check

--- web_flask/9-states.py
+++ web_flask/9-states.py
@@ -1,29 +1,24 @@
 #!/usr/bin/python3
-"""
-This module starts a Flask web application
-"""
+""" initializes and configures a Flask web application"""
 
 from flask import Flask, render_template
-from models import *
 from models import storage
-app = Flask(__name__)
 
+flask_app = Flask(__name__)
 
-@app.route('/states', strict_slashes=False)
-@app.route('/states/<state_id>', strict_slashes=False)
-def states(state_id=None):
-    """This function display the states and cities
-    listed in alphabetical order"""
-    states = storage.all("State")
-    if state_id is not None:
-        state_id = 'State.' + state_id
-    return render_template('9-states.html', states=states, state_id=state_id)
+@flask_app.route('/states', strict_slashes=False)
+@flask_app.route('/states/<id>', strict_slashes=False)
+def get_states(id=None):
+    """Display states and cities listed in alphabetical order."""
+    s = storage.all("State")
+    if id is not None:
+        id = 'State.' + id
+    return render_template('9-states.html', state=s, id=id)
 
-
-@app.teardown_appcontext
-def teardown_db(exception):
-    """This function closes the storage on teardown"""
+@flask_app.teardown_appcontext
+def close_db(exep):
+    """Close the db"""
     storage.close()
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port='5000')
+    flask_app.run(host='0.0.0.0', port='5000')
