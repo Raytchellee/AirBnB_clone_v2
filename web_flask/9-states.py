f212feadf42019fcd3c57b0055cfa6729d191907
Ray Ray <101654980+Raytchellee@users.noreply.github.com>
2023-11-26 01:56:35 +0100

    Update 9-states.py

--- web_flask/9-states.py
+++ web_flask/9-states.py
@@ -1,32 +1,27 @@
 #!/usr/bin/python3
-"""renders html files"""
+"""Renders State values"""
+from models import *
 from models import storage
 from flask import Flask, render_template
+
 
 flask_app = Flask(__name__)
 
 
-@flask_app.route("/states", strict_slashes=False)
-def get_state():
-    """gets the state"""
+@flask_app.route('/states', strict_slashes=False)
+@flask_app.route('/states/<state_id>', strict_slashes=False)
+def get_state(id=None):
+    """Renders available states"""
     s = storage.all("State")
-    return render_template("9-states.html", state=s)
-
-
-@flask_app.route("/states/<id>", strict_slashes=False)
-def get_cities(id):
-    """gets cities of a given id"""
-    for s in storage.all("State").values():
-        if s.id == id:
-            return render_template("9-states.html", state=s)
-    return render_template("9-states.html")
+    if id is not None:
+        id = 'State.' + id
+    return render_template('9-states.html', id=id, state=s)
 
 
 @flask_app.teardown_appcontext
 def reloads(exep):
-    """reloads all"""
+    """reloads the state"""
     storage.close()
 
-
-if __name__ == "__main__":
-    flask_app.run(host="0.0.0.0")
+if __name__ == '__main__':
+    flask_app.run(host='0.0.0.0', port='5000')
